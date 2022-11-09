0de234b1e025dcdd441b8959a2d62ae2ba8596b5
rafhaella <rafhaellamoreno@gmail.com>
2022-11-09 15:43:18 -0300

    Up new files for the course Introduction Dart

---
+++ lab01/hello-function.dart
@@ -0,0 +1,14 @@
+import '../lab03/solutions/hello-function.dart';
+
+void main() {
+  bool isDartCool = isDartCoolFunc();
+  if (isDartCool) {
+    print('Hello $isDartCool');
+  } else {
+    print('Hmm... I think Dart is pretty cool!');
+  }
+  }
+  bool isDartCoolFunc() {
+    bool isDartCool = true;
+    return isDartCool;
+}
---
+++ lab01/hello-string.dart
@@ -0,0 +1,4 @@
+void main() {
+  String getCourseName = "flutter bootcamp 22";
+  print('Hello $getCourseName');
+}
